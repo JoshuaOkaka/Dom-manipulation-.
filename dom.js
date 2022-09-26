468e03957e11cbad4560141ae90f00a9b76e613c
joshua <joshuaokaka@protonmail.com>
2022-09-26 10:07:34 +0100

    played with the five selector object

--- dom.js
+++ dom.js
@@ -2,15 +2,27 @@
 
 
 //getElementById()
+const title = document.getElementById('heading');
+console.log(title);
+
+
 
 
 //getElementByClassName()
-
+const listItem = document.getElementsByClassName('list-item')
+console.log(listItem)
 
 // getElementByTagName()
 
+const tagName = document.getElementsByTagName('li');
+console.log(tagName);
+ 
 
 //querySelector()
+const qselector = document.querySelector('div');
+console.log(qselector)
 
 
 //querySelectorAll()
+const qselectorAll = document.querySelectorAll('div')
+console.log(qselectorAll)
